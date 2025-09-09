df075f546c97c14f5a80c7005a8cdca3c09cf7f7
Shohrux <shohruxnizomov170@gmail.com>
2025-09-09 19:49:28 +0500

    admin roli kiritildi

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 });
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::resource('categories', \App\Http\Controllers\CategoryController::class);
+});
+
 
 require __DIR__.'/auth.php';
