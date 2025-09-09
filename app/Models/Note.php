34c9155333ee2f0477bfdb4fc31f5eed290c3c8e
Shohrux <shohruxnizomov170@gmail.com>
2025-09-09 19:17:29 +0500

    categorylarani bazaga saqlash un migratsiya va model yozildi

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -13,4 +13,10 @@ class Note extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
